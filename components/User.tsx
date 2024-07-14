0af9e58ecbae5936811ff48041bd75de35348be4
Satyark <satyark351@gmail.com>
2024-07-14 19:05:19 +0530

    Fixed css

--- components/User.tsx
+++ components/User.tsx
@@ -13,12 +13,12 @@ interface UserProps {
 const User: React.FC<UserProps> = ({ id, name, email, EmailAddress, job, phone, company }) => {
   return (
     <div className="p-4 border rounded-lg shadow-md bg-white hover:shadow-lg transition">
-      <h2 className="text-black font-bold">{name}</h2>
-      <p className="text-gray-600">Job Title: {job}</p>
-      <p className="text-gray-600">Company: {company}</p>
-      <p className="text-gray-600">Phone: {phone}</p>
-      <p className="text-gray-600">Email: {email}</p>
-      <p className="text-gray-600">Email Address: {EmailAddress}</p>
+      <h2 className="text-black font-bold break-words">{name}</h2>
+      <p className="text-gray-600 break-words">Job Title: {job}</p>
+      <p className="text-gray-600 break-words">Company: {company}</p>
+      <p className="text-gray-600 break-words">Phone: {phone}</p>
+      <p className="text-gray-600 break-words">Email: {email}</p>
+      <p className="text-gray-600 break-words">Email Address: {EmailAddress}</p>
       </div>
   
   );
